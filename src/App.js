77098008ce47c289e0038b0c2bfb3edc8f03a0c2
David-Zephyrus <dbekosmas@gmail.com>
2024-11-26 22:44:33 -0500

    Limpia elementos de plantilla React.

--- src/App.js
+++ src/App.js
@@ -6,20 +6,7 @@ function App() {
   return (
     <div className="App">
       <Header />  {/* Tercera forma de llamar un componente de React. */}
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hola Mundo con React.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprende React
-        </a>
-      </header>
+      
     </div>
   );
 }
