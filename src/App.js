2fc4f8d407501459034017ca1a9c4a6735b5a76f
David-Zephyrus <dbekosmas@gmail.com>
2024-11-27 20:00:06 -0500

    Se crea esqueleto del componente Formulario.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+
+/* Importación de Componentes: */
 import Header from './componentes/Header/Header.js';
+import Formulario from './componentes/Formulario/Formulario.js';
 
 function App() {
   return (
     <div>
       <Header />  {/* Tercera forma de llamar un componente de React. */}
-      
+      <Formulario />
     </div>
   );
 }
