01bddb5122f7481e089a2bc3da79cec70d3de170
David-Zephyrus <dbekosmas@gmail.com>
2024-11-27 21:43:56 -0500

    Se crea y se da estilo al componente CampoTexto.

--- src/componentes/Formulario/Formulario.js
+++ src/componentes/Formulario/Formulario.js
@@ -1,9 +1,13 @@
 import "./Formulario.css";
+import CampoTexto from "../CampoTexto";
 
 const Formulario = () => {
     return <section> 
         <form className="formulario">
             <h2> Rellena el formulario para crear el colaborador. </h2>
+            <CampoTexto />
+            <CampoTexto />
+            <CampoTexto />
         </form>
     </section>
 };
