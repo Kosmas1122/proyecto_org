0c8d6f7e334cf409a81fd357be84dd5779d5995a
David-Zephyrus <dbekosmas@gmail.com>
2024-12-03 21:53:32 -0500

    Vinculando evento click a un componente:
    Se vincula el evento onClick al componente MiOrg cuando se
    haga click en el elemento de imagen add.png
    Se lanza un mensaje a consola para verificar el funcionamiento
    del evento.

--- src/componentes/MiOrg/index.js
+++ src/componentes/MiOrg/index.js
@@ -1,9 +1,14 @@
 import "./MiOrg.css";
 
 const MiOrg = () => {
+
+    const manejarClick = () => {
+        console.log("Mostrar/Ocultar elemento");
+    }
+
     return <section className="orgSection">
         <h3 className="title"> Mi Organización </h3>
-        <img src="/img/add.png" alt="add" />
+        <img src="/img/add.png" alt="add" onClick={manejarClick} />
     </section>
 };
 
