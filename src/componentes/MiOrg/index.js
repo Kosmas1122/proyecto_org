8a97e3353016d18b371ed7f24fee8b91ba1801aa
David-Zephyrus <dbekosmas@gmail.com>
2024-12-03 22:22:15 -0500

    Uso de Estados y Hooks.

--- src/componentes/MiOrg/index.js
+++ src/componentes/MiOrg/index.js
@@ -1,13 +1,22 @@
+import { useState } from "react";
 import "./MiOrg.css";
 
 const MiOrg = () => {
+    // Estado - Hooks:
+    // useState
+    // useState()
+    console.log()
+    // const [nombreVariable, funcionActualiza] = useState(valorInicial)
+    const [nombre, actualizarNombre] = useState("Harland");
+    const [mostrar, actualizarMostrar] = useState(true);
 
     const manejarClick = () => {
-        console.log("Mostrar/Ocultar elemento");
+        console.log("Mostrar/Ocultar elemento", !mostrar);
+        actualizarMostrar(!mostrar);
     }
 
     return <section className="orgSection">
-        <h3 className="title"> Mi Organización </h3>
+        <h3 className="title"> Mi Organización {nombre} </h3>
         <img src="/img/add.png" alt="add" onClick={manejarClick} />
     </section>
 };
