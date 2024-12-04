64ff312de7e612fef5f0d4850f9d52147d37053f
David-Zephyrus <dbekosmas@gmail.com>
2024-12-03 21:46:51 -0500

    Añade componente MiOrg

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 /* Importación de Componentes: */
 import Header from './componentes/Header/Header.js';
 import Formulario from './componentes/Formulario/Formulario.js';
+import MiOrg from './componentes/MiOrg';
 
 function App() {
   return (
     <div>
       <Header />  {/* Tercera forma de llamar un componente de React. */}
       <Formulario />
+      <MiOrg />
     </div>
   );
 }
