eaa443b85982e8cb7a3f6c210aa389815b9e36a3
David-Zephyrus <dbekosmas@gmail.com>
2024-12-05 19:35:24 -0500

    Controlando los inputs:
    Se crean los estados necesarios para controlar los inputs desde
    el componente Formulario.

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,11 +1,33 @@
+import { useState } from "react";
 import "./CampoTexto.css";
 
 const CampoTexto = (props) => {
-    console.log("Datos: ", props);
+    //const [valor, setValor] = useState("");
+    //console.log("Datos: ", props);
+
+    const manejarCambio = (e) => {
+        //console.log("Cambio", e.target.value);
+        props.actualizarValor(e.target.value);
+    }
+
     return <div className="campo-texto">
         <label> {props.titulo} </label>
-        <input placeholder={`${props.placeholder}...`} required={props.required} />
+        <input 
+            placeholder={`${props.placeholder}...`} 
+            required={props.required} 
+            value={props.valor} 
+            onChange={manejarCambio}
+        />
     </div>
 };
+
+/* 
+NOTA:
+Se acostumbra usar como nombre de función para 
+el estado, la palabra set+nombreVariable, así:
+const [valor, setValor] = ...
+Puede usarse cualquier nombre, pero lo anterior
+es una costumbre dentro del desarrollo en React.
+*/
 
 export default CampoTexto;
--- src/componentes/Formulario/Formulario.js
+++ src/componentes/Formulario/Formulario.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./Formulario.css";
 import CampoTexto from "../CampoTexto";
 import ListaOpciones from "../ListaOpciones"
@@ -5,17 +6,60 @@ import Boton from "../Boton";
 
 const Formulario = () => {
 
+    /* Estados: */
+    const [nombre, setNombre] = useState("");
+    const [puesto, setPuesto] = useState("");
+    const [foto, setFoto] = useState("");
+
+
     const manejarEnvio = (e) => {
         e.preventDefault();
-        console.log("Manejar el envío", e);
+        console.log("Manejar el envío");
+
+        // Se crea un objeto para guardar los datos del formulario:
+        let datosAEnviar = {
+            nombre: nombre,
+            puesto: puesto,
+            foto: foto
+        };
+        console.log(datosAEnviar);
+
+        /* El objeto anterior también se pudo haber escrito así:
+            let datosAEnviar = {
+                nombre,
+                puesto,
+                foto
+            };
+
+            Internamente JS sabe que la clave y el valor tendrán
+            el mismo nombre.
+        */
     }
 
     return <section className="formulario"> 
         <form onSubmit={manejarEnvio}>
             <h2> Rellena el formulario para crear el colaborador. </h2>
-            <CampoTexto titulo="Nombre" placeholder="Ingresar nombre" required={true} />
-            <CampoTexto titulo="Puesto" placeholder="Ingresar puesto" required /> 
-            <CampoTexto titulo="Foto"   placeholder="Ingresar enlace de foto" required />
+            <CampoTexto 
+                titulo="Nombre" 
+                placeholder="Ingresar nombre" 
+                required={true} 
+                valor={nombre} 
+                actualizarValor={setNombre}
+            />
+            <CampoTexto 
+                titulo="Puesto" 
+                placeholder="Ingresar puesto" 
+                required 
+                valor={puesto} 
+                actualizarValor={setPuesto}
+            /> 
+            <CampoTexto 
+                titulo="Foto" 
+                placeholder="Ingresar enlace de foto" 
+                required
+                valor={foto} 
+                actualizarValor={setFoto}
+            />
             <ListaOpciones />
             <Boton texto="Crear"/>
         </form>
