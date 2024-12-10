f1b450f27e89625995600982c73dec0e63a08f47
David-Zephyrus <dbekosmas@gmail.com>
2024-12-09 22:34:51 -0500

    Creando Componente tarjeta Colaborador

--- src/componentes/Equipo/index.js
+++ src/componentes/Equipo/index.js
@@ -1,4 +1,5 @@
-import "./Equipo.css"
+import "./Equipo.css";
+import Colaborador from "../Colaborador";
 
 const Equipo = (props) => {
     // Concepto de Destructuración:
@@ -11,7 +12,10 @@ const Equipo = (props) => {
     return <section className="equipo" style={obj} >
         <h3 style={estiloTitulo} > {titulo} </h3>
         <div className="colaboradores">
-
+            <Colaborador />
+            <Colaborador />
+            <Colaborador />
+            <Colaborador />
         </div>
     </section>
 };
