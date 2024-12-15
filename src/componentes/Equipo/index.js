a16aa138b61953419f26b0cbe11e2274f9f8641b
David-Zephyrus <dbekosmas@gmail.com>
2024-12-15 17:39:12 -0500

    Color de fondo de Equipo.

--- src/componentes/Equipo/index.js
+++ src/componentes/Equipo/index.js
@@ -18,11 +18,16 @@ const Equipo = (props) => {
                 <h3 style={estiloTitulo} > {titulo} </h3>
                 <div className="colaboradores">
                     {
-                        colaboradores.map((colaborador, index) => <Colaborador datos={colaborador} key={index} />)
+                        colaboradores.map((colaborador, index) => <Colaborador 
+                            datos={colaborador} 
+                            key={index} 
+                            colorPrimario={colorPrimario} 
+                        />)
                     }
                 </div>
             </section>
-        } </>
+        } 
+        </>
 };
 
 export default Equipo;
