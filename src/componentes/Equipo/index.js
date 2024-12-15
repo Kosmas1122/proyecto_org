990292f87e515aa76dec78b67c288219523a2053
David-Zephyrus <dbekosmas@gmail.com>
2024-12-15 17:28:43 -0500

    Filtrando Equipos.

--- src/componentes/Equipo/index.js
+++ src/componentes/Equipo/index.js
@@ -8,16 +8,21 @@ const Equipo = (props) => {
 
     const obj = { backgroundColor: colorSecundario }
 
+    console.log(colaboradores.length > 0);
+
     const estiloTitulo = { borderColor: colorPrimario }
 
-    return <section className="equipo" style={obj} >
-        <h3 style={estiloTitulo} > {titulo} </h3>
-        <div className="colaboradores">
-            {
-                colaboradores.map((colaborador, index) => <Colaborador datos={colaborador} key={index} />)
-            }
-        </div>
-    </section>
+    return <>
+        {colaboradores.length > 0 &&
+            <section className="equipo" style={obj} >
+                <h3 style={estiloTitulo} > {titulo} </h3>
+                <div className="colaboradores">
+                    {
+                        colaboradores.map((colaborador, index) => <Colaborador datos={colaborador} key={index} />)
+                    }
+                </div>
+            </section>
+        } </>
 };
 
 export default Equipo;
