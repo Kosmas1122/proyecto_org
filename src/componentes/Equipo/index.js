f6ad46681022114c82295f9e58cca9b6b81371a0
David-Zephyrus <dbekosmas@gmail.com>
2024-12-16 19:46:54 -0500

    Agregando input color.

--- src/componentes/Equipo/index.js
+++ src/componentes/Equipo/index.js
@@ -8,13 +8,19 @@ const Equipo = (props) => {
 
     const obj = { backgroundColor: colorSecundario }
 
-    console.log(colaboradores.length > 0);
-
     const estiloTitulo = { borderColor: colorPrimario }
 
     return <>
         {colaboradores.length > 0 &&
             <section className="equipo" style={obj} >
+                <input 
+                    type='color'
+                    className="input-color"
+                    value={colorSecundario}
+                    onChange={(evento) => {
+                        console.log(evento.target.value);
+                    }}
+                />
                 <h3 style={estiloTitulo} > {titulo} </h3>
                 <div className="colaboradores">
                     {
